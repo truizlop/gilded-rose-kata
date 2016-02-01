34647fde998f9b3bc0e45f5067983e91ecf0808c
Tomas Ruiz-Lopez <trl.ironside@gmail.com>
2016-02-01 18:03:22 +0100

    Extract quality updater interface

--- src/main/java/com/truizlop/gildedrose/quality/DefaultQualityUpdater.java
+++ src/main/java/com/truizlop/gildedrose/quality/DefaultQualityUpdater.java
@@ -2,8 +2,9 @@ package com.truizlop.gildedrose.quality;
 
 import com.truizlop.gildedrose.Item;
 
-public class DefaultQualityUpdater {
+public class DefaultQualityUpdater implements QualityUpdater {
 
+    @Override
     public void update(Item item) {
         if(item.isExpired()){
             item.decreaseQualityBy(2);
---
+++ src/main/java/com/truizlop/gildedrose/quality/QualityUpdater.java
@@ -0,0 +1,7 @@
+package com.truizlop.gildedrose.quality;
+
+import com.truizlop.gildedrose.Item;
+
+public interface QualityUpdater {
+    void update(Item item);
+}
