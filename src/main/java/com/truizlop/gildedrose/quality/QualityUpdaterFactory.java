6f43ead083d67ede5b49062e33a99d3adef187f5
Tomas Ruiz-Lopez <trl.ironside@gmail.com>
2016-02-02 10:54:43 +0100

    Add factory to provide quality updaters depending on item type

---
+++ src/main/java/com/truizlop/gildedrose/quality/QualityUpdaterFactory.java
@@ -0,0 +1,22 @@
+package com.truizlop.gildedrose.quality;
+
+import com.truizlop.gildedrose.ItemType;
+
+public class QualityUpdaterFactory {
+
+    public QualityUpdater makeUpdaterFor(ItemType type){
+
+        switch (type){
+            case AGED_BRIE:
+                return new AgedBrieQualityUpdater();
+            case BACKSTAGE:
+                return new BackstageQualityUpdater();
+            case SULFURAS:
+                return new SulfurasQualityUpdater();
+            default:
+                return new DefaultQualityUpdater();
+        }
+
+    }
+
+}
---
+++ src/test/java/com/truizlop/gildedrose/quality/QualityUpdaterFactoryTest.java
@@ -0,0 +1,50 @@
+package com.truizlop.gildedrose.quality;
+
+import com.truizlop.gildedrose.ItemType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.truizlop.gildedrose.ItemType.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+
+public class QualityUpdaterFactoryTest {
+    private static final ItemType ANY_ITEM = DEXTERITY_VEST;
+
+    private QualityUpdaterFactory qualityUpdaterFactory;
+
+    @Before
+    public void initialize(){
+        qualityUpdaterFactory = new QualityUpdaterFactory();
+    }
+
+    @Test
+    public void shouldProvideQualityUpdaterForDefaultItem(){
+        QualityUpdater qualityUpdater = qualityUpdaterFactory.makeUpdaterFor(ANY_ITEM);
+
+        assertThat(qualityUpdater, is(instanceOf(DefaultQualityUpdater.class)));
+    }
+
+    @Test
+    public void shouldProvideAgedBrieUpdaterForAgedBrieItem(){
+        QualityUpdater qualityUpdater = qualityUpdaterFactory.makeUpdaterFor(AGED_BRIE);
+
+        assertThat(qualityUpdater, is(instanceOf(AgedBrieQualityUpdater.class)));
+    }
+
+    @Test
+    public void shouldProvideBackstageUpdaterForBackstageItem(){
+        QualityUpdater qualityUpdater = qualityUpdaterFactory.makeUpdaterFor(BACKSTAGE);
+
+        assertThat(qualityUpdater, is(instanceOf(BackstageQualityUpdater.class)));
+    }
+
+    @Test
+    public void shouldProvideSulfurasUpdaterForSulfurasItem(){
+        QualityUpdater qualityUpdater = qualityUpdaterFactory.makeUpdaterFor(SULFURAS);
+
+        assertThat(qualityUpdater, is(instanceOf(SulfurasQualityUpdater.class)));
+    }
+
+}
